a337751844e58686fec66f8595ad0cf5f5d06997
NachosChipeados <103285866+NachosChipeados@users.noreply.github.com>
2026-01-12 14:01:14 -0400

    Add addon_hd game directory

--- filesystem/filesystem.c
+++ filesystem/filesystem.c
@@ -1331,6 +1331,12 @@ void FS_AddGameHierarchy( const char *dir, uint flags )
 		FS_AddGameDirectory( buf, flags|FS_NOWRITE_PATH|FS_CUSTOM_PATH );
 	}
 
+	if( FBitSet( flags, FS_MOUNT_HD ) && FBitSet( flags, FS_MOUNT_ADDON ) )
+	{
+		Q_snprintf( buf, sizeof( buf ), "%s_addon_hd/", dir );
+		FS_AddGameDirectory( buf, flags|FS_NOWRITE_PATH|FS_CUSTOM_PATH );
+	}
+
 	if( FBitSet( flags, FS_MOUNT_LV ))
 	{
 		Q_snprintf( buf, sizeof( buf ), "%s_lv/", dir );
